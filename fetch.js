f552de67427438b2ae72187b848cecdb8b9a9007
Mubassir Habib <66573095+Mubassir1820@users.noreply.github.com>
2024-05-21 11:06:02 +0600

    setup js file

--- fetch.js
+++ fetch.js
@@ -1,1 +1,13 @@
-console.log('hello')
+fetch('http://api.weatherapi.com/v1/current.json?key=75eed3fc01164f22b72135905242005&q=London&aqi=no')
+    .then(response => {
+        if(!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Data received', data);
+    })
+    .catch(error => {
+        console.error('There was a problem with fetch operation: ', error);
+    });
